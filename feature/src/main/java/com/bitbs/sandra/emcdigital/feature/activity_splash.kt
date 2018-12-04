d24b3a0188c6e29efe79cfbba1edad7c1bdf458d
victor-granados <45583258+victor-granados@users.noreply.github.com>
2018-12-03 23:27:47 -0600

    Primera Versión no funcional

---
+++ feature/src/main/java/com/bitbs/sandra/emcdigital/feature/activity_splash.kt
@@ -0,0 +1,13 @@
+package com.bitbs.sandra.emcdigital.feature
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
+
